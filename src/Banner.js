64bb92a5d34913973b318c8a60644c888ee79307
Konstantinos K <koufogiannakiskon@gmail.com>
2022-11-15 22:13:52 +0200

    start of banner

--- src/Banner.js
+++ src/Banner.js
@@ -1,14 +1,16 @@
 import React, { useState } from 'react'
+import axios from './axios';
+import requests from './requests';
 
 function Banner() {
     const [movie, setMovies] = useState([]);
 
     useEffect(() => {
         async function fetchData() {
-
+            const request = await axios.get(requests.fetchTrending);
+            setMovie(request.data.results[Math.floor(Math.random() * request.data.results.length - 1)]);
         }
         fetchData();
-
     }, []);
 
     return (
